66ba8de06f798c22392a644df3127a3e6d27bbab
asif0 <asif000473@gmail.com>
2019-04-15 16:32:58 +1000

    Initial Commit

---
+++ app/src/main/java/com/example/trustrecorder/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.trustrecorder;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.users, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
