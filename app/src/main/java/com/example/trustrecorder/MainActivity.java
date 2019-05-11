a579ad86800feb9bb0073b18f98316f435978021
asif0 <asif000473@gmail.com>
2019-05-11 21:22:20 +1000

    Changes to main activity and layout.

--- app/src/main/java/com/example/trustrecorder/MainActivity.java
+++ app/src/main/java/com/example/trustrecorder/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.trustrecorder;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
+
     private TextView mTextView;
+    Spinner spn1;
+    TextView user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +24,50 @@ public class MainActivity extends WearableActivity {
         setContentView(R.layout.activity_main);
 
         mTextView = (TextView) findViewById(R.id.text);
+        spn1 = (Spinner) findViewById(R.id.spn1);
+        user = (TextView) findViewById(R.id.user);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.users, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+        spn1.setAdapter(adapter);
 
         // Enables Always-on
         setAmbientEnabled();
+
+        spn1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                switch(position){
+                    case 0:
+                        user.setText("Welcome");
+                        break;
+                    case 1:
+                        user.setText("Welcome Asif");
+                        break;
+                    case 2:
+                        user.setText("Welcome Nadim");
+                        break;
+                    case 3:
+                        user.setText("Welcome Sang");
+                        break;
+                    case 4:
+                        user.setText("Welcome Apu");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        }
+        );
     }
 
     public void sendMessage(View view)
@@ -38,4 +76,8 @@ public class MainActivity extends WearableActivity {
         startActivity(intent);
     }
 
+
+
 }
+
+
